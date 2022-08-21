48611d05cba221b65eb6a0c06c02c94342fc44eb
blin007 <blin00712@gmail.com>
2022-08-20 20:45:14 -0400

    8/20 began work on home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import '../../styles/Home.css'
+import HomeNavbar from './components/HomeNavbar'
 
 function Home() {
   return (
     <div className="home">
+        <HomeNavbar />
+
         <img 
             className="home_banner_top"
             src="https://cdn.cloudflare.steamstatic.com/steam/clusters/frontpage/9aeb2c71424deef0062b0449/page_bg_english.jpg?t=1660348764"
---
+++ src/pages/Home/components/HomeNavbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../../../styles/Home.css'
+
+function HomeNavbar() {
+  return (
+    <div className="home_navbar">
+        <div className="home_navarea">
+            <div className="home_navbackground">
+                <div className="home_navContent">
+                    <div className="tab">
+                        <span class="item">Lorem ipsum</span>
+                    </div>
+                    <div className="tab">
+                        <span class="item">Lorem ipsum</span>
+                    </div>
+                    <div className="tab">
+                        <span class="item">Lorem ipsum</span>
+                    </div>
+                    <div className="tab">
+                        <span class="item">Lorem ipsum</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HomeNavbar
