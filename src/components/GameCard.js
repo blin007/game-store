06bc8e31b2bd1482387225b5cb2ffd5376910a1a
blin007 <blin00712@gmail.com>
2022-08-21 19:00:07 -0400

    8/21 added animations to home page cards

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,30 +1,86 @@
 import React from 'react'
 import '../styles/GameCard.css'
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { FaWindows, FaApple, FaXbox, FaPlaystation} from 'react-icons/fa';
 
-function GameCard({ image, title, price, windows, apple, xbox, playstation }) {
+const variants = {
+    hidden: direction => {
+        return { 
+            x: direction > 0 ? 500 : -500,
+            opacity: 0,
+            scale: 0.5,
+        }
+    },
+    visible: {
+        x: 0,
+        opacity: 1,
+        scale: 1,
+        transition: {
+            x: {
+                type: 'spring',
+                stiffness: 300,
+                damping: 30,
+                duration: 0.01
+            },
+            opacity: {
+                duration: .1,
+            }
+        }
+    },
+    exit: direction => {
+        return {
+            x: direction > 0 ? -300 : 300,
+            opacity: 0,
+            scale: 0.5,
+            transition: {
+                x: {
+                    type: 'spring',
+                    stiffness: 300,
+                    damping: 30,
+                    duration: 0.01,
+                },
+                opacity: {
+                    duration: .2,
+                }
+            }
+        }
+    }
+}
+
+function GameCard({ image, title, price, windows, apple, xbox, playstation, direction }) {
   return (
-    <motion.div 
-        className="card"
-        whileHover={{cursor: 'pointer'}}    
-    >
-        <img src={image} alt="slide" className="slide" />
-        <div className="slide_info">
-            <span className="game_title">ELDEN RING</span>
-            <span>Available Now!</span>
-            <span>Top Seller!</span>
-            <div className="game_price">$59.99</div>
-            <div className="platforms">
-                {windows && (<FaWindows className="platform" />)}
-                {apple && (<FaApple className="platform"/>)}
-                {xbox && (<FaXbox className="platform"/>)}
-                {playstation && (<FaPlaystation className="platform"/>)}
-            </div>
-        </div>   
+    <AnimatePresence initial={false} mode="wait" custom={direction}>
+        <motion.div 
+            className="card"
+            whileHover={{cursor: 'pointer', boxShadow: "0 0 20px 0px rgb(1, 7, 27)"}}    
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            key={image}
+            custom={direction}
+        >
+            <img 
+                src={image} 
+                alt="slide" 
+                id="img"
+                className="slide" 
+            />
+            <div id="slide_info" className="slide_info">
+                <span className="game_title">ELDEN RING</span>
+                <span>Available Now!</span>
+                <span>Top Seller!</span>
+                <div className="game_price">$59.99</div>
+                <div className="platforms">
+                    {windows && (<FaWindows className="platform" />)}
+                    {apple && (<FaApple className="platform"/>)}
+                    {xbox && (<FaXbox className="platform"/>)}
+                    {playstation && (<FaPlaystation className="platform"/>)}
+                </div>
+            </div>   
 
-    </motion.div>
-
+        </motion.div>
+    </AnimatePresence>
   )
 }
 
--- src/pages/Home/components/Slideshow.js
+++ src/pages/Home/components/Slideshow.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../../styles/Slideshow.css'
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import GameCard from '../../../components/GameCard';
+import { motion } from 'framer-motion';
 
 const images = [
     "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/capsule_616x353.jpg?t=1654259241",
@@ -13,14 +14,42 @@ const images = [
 ]
 
 function Slideshow() {
+
+    const [index, setIndex] = useState(0);
+    const [direction, setDirection] = useState(0);
+
+    const moveSlide = (e, step) => {
+        if (step > 0) setDirection(1)
+        else setDirection(-1)
+        if (step=== 1 && index === images.length - 1){
+            setIndex(0);
+            return;
+        }
+        else if (step=== -1 && index === 0) {
+            setIndex(images.length - 1);
+            return;
+        }
+        setIndex(index + step)
+    }
+
   return (
     <div className="slideshow_container">
         <div className="slideshow">
             <div className="slideshow_items">
-                <GameCard image={images[0]} windows={true} apple={true} xbox={true} playstation={true}/>
+                <GameCard image={images[index]} windows={true} apple={true} xbox={true} playstation={true} direction={direction}/>
             </div>
-            <button className="prev_button"><NavigateBeforeIcon fontSize='large' className="arrow"/></button>
-            <button className="next_button"><NavigateNextIcon fontSize='large' className="arrow"/></button>
+            <motion.button className="prev_button"
+                onClick={e => moveSlide(e, -1)}
+                whileHover={{background: "linear-gradient(to right, rgba(255,255,255,0.3) , rgba(0,0,0,0))"}}
+            >
+                <NavigateBeforeIcon fontSize='large' className="arrow"/>
+            </motion.button>
+            <motion.button className="next_button"
+                onClick={e => moveSlide(e, 1)}
+                whileHover={{background: "linear-gradient(to left, rgba(255,255,255,0.3) , rgba(0,0,0,0))"}}
+            >
+                <NavigateNextIcon fontSize='large' className="arrow"/>
+            </motion.button>
         </div>
     </div>
 
