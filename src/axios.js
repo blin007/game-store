6980407ab5939e7fcdaed5d53ac50d6be0dd9011
Brian Lin <blin00712@gmail.com>
2022-08-29 23:17:26 -0400

    8/29 modified axios baseurl

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const instance= axios.create({
-    baseURL: 'http://localhost:5001/game-store-589e9/us-central1/api' //local api endpoint
+    baseURL: 'https://us-central1-game-store-589e9.cloudfunctions.net/api' //local api endpoint
+    //Local api endpoint: for testing: http://localhost:5001/game-store-589e9/us-central1/api
 });
 
 export default instance;
